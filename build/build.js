7b03ce73e7ffa82c1e195b647f2245e13d24c8ec
景天 <jingtian@naitang.com>
2016-07-05 13:32:24 +0800

    *

--- build/build.js
+++ build/build.js
@@ -22,7 +22,8 @@ spinner.start()
 var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
 rm('-rf', assetsPath)
 mkdir('-p', assetsPath)
-cp('-R', 'static/', assetsPath)
+console.log(assetsPath)
+cp('-R', 'static/*', assetsPath)
 
 webpack(webpackConfig, function(err, stats) {
     spinner.stop()
